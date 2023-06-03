499e1a54e274a7c7e61cbdafda4b10246eff67ad
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-06-03 19:55:10 -0400

    wdawdwd

--- resources/views/inicio/ingresar.blade.php
+++ resources/views/inicio/ingresar.blade.php
@@ -8,9 +8,11 @@
     <title>Universidad Lorem Ipsum</title>
 </head>
 <body>
-    
-    pase los datos :3
-    asdhasjkdjashk
+    <div class="container">
+        <div class="col">
+             qwdqwdqwdqwdqw
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
