daf41c46f27e906168c4f103a89f6cfcacf18606
Bastian <bastian.rojash@usm.cl>
2023-06-03 19:08:50 -0400

    Titulos
    Titulos para la pestaña

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('paleta-colores/paleta.css')}}">
-    <title>Document</title>
+    <title>Universidad Lorem Ipsum</title>
 </head>
 <body class="bg-light">
     <div class="container-fluid min-vh-100 d-flex flex-column justify-content-lg-center">
