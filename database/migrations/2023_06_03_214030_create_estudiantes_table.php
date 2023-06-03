55f0cabb1789e8e98bd3d64c1cd388fce64efb33
Bastian <bastian.rojash@usm.cl>
2023-06-03 18:18:50 -0400

    Probando como funciona git
    Estoy probando nomas :3

--- database/migrations/2023_06_03_214030_create_estudiantes_table.php
+++ database/migrations/2023_06_03_214030_create_estudiantes_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('estudiantes', function (Blueprint $table) {
             $table->id();
+            $table->timestamps();
         });
     }
 
