735ec7098d51ab0eecb3e90607844fe3ef29d4d6
Bastian <bastian.rojash@usm.cl>
2023-06-04 23:10:12 +0000

    Jun 4, 2023, 7:10 PM

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Estudiante;
 
 class EstudianteController extends Controller
 {
+    public function iniciar(Request $request){
+        return redirect()->route('estudiante.menu',$request->id);
+    }
+
     public function index(){
         return view('estudiante.index');
     }
