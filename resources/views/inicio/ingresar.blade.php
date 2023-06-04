5f769a02643c6cb930ff8a83c5f2f305f3b2bee1
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-06-04 02:28:18 +0000

    Jun 3, 2023, 10:28 PM

--- resources/views/inicio/ingresar.blade.php
+++ resources/views/inicio/ingresar.blade.php
@@ -11,32 +11,34 @@
     <div class="container-fluid min-vh-100 d-flex flex-column justify-content-lg-center bg-light">
         <div class="row">
             <div class="col-5 offset-1 d-flex bg-primary">
-                <h4 class="text-white">Inicio de Sesión</h4>
-                <h5 class="text-white my-2">Ingrese sus datos</h5>
-                <div class="card">
-                    <div class="card-body">
-                        <form>
-                            <div class="mb-3">
-                                <label for="username" class="form-label">Nombre de Usuario</label>
-                                <input type="text" id="username" class="form-control">
-                            </div>
-                            <div class="mb-3">
-                                <label for="password" class="form-label">Contraseña</label>
-                                <input type="password" id="password" class="form-control">
-                            </div>
-                            <div class="row mb-2">
-                                <div class="col-6">
-                                    <a href="#" class="btn btn-secondary">Iniciar Sesión</a>
+                <div>
+                    <h4 class="text-white">Inicio de Sesión</h4>
+                    <h5 class="text-white my-2">Ingrese sus datos</h5>
+                    <div class="card">
+                        <div class="card-body">
+                            <form>
+                                <div class="mb-3">
+                                    <label for="username" class="form-label">Nombre de Usuario</label>
+                                    <input type="text" id="username" class="form-control">
                                 </div>
-                                <div class="col-6">
-                                    <small><a href="{{route('inicio.index')}}" class="text-secondary d-flex justify-content-end pt-3">Volver</a></small>
+                                <div class="mb-3">
+                                    <label for="password" class="form-label">Contraseña</label>
+                                    <input type="password" id="password" class="form-control">
                                 </div>
-                            </div>
-                        </form>
+                                <div class="row mb-2">
+                                    <div class="col-6">
+                                        <a href="#" class="btn btn-secondary">Iniciar Sesión</a>
+                                    </div>
+                                    <div class="col-6">
+                                        <small><a href="{{route('inicio.index')}}" class="text-secondary d-flex justify-content-end pt-3">Volver</a></small>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-5 d-flex">
+            <div class="col-5 d-flex bg-primary">
                 <div>
                     <h2 class="text-white">Universidad Lorem Ipsum</h2>
                 </div>
