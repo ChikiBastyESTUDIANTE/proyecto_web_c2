e60c9af8f97fec76285a875d7acf9c6f75e0e2cf
Bastian <bastian.rojash@usm.cl>
2023-06-04 00:52:56 +0000

    Jun 3, 2023, 8:52 PM

--- app/Models/Propuesta.php
+++ app/Models/Propuesta.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Propuesta extends Model
 {
     use HasFactory;
+    protected $table = 'propuestas';
+
+    public function proponedor():HasMany{
+        return $this->HasMany(Estudiante::class);
+    }
 }
