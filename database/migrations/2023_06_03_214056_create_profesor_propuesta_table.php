bb803f1dd37810b56dba8ac3eecb797c530f4cdc
Bastian <bastian.rojash@usm.cl>
2023-06-04 01:09:42 +0000

    Jun 3, 2023, 9:09 PM

--- database/migrations/2023_06_03_214056_create_profesor_propuesta_table.php
+++ database/migrations/2023_06_03_214056_create_profesor_propuesta_table.php
@@ -14,13 +14,13 @@ return new class extends Migration
         Schema::create('profesor_propuesta', function (Blueprint $table) {
             //tipos de datos
             $table->integer('propuesta_id'); //primary key
-            $table->ingeter('profesor_id'); //primary key
+            $table->integer('profesor_id'); //primary key
             $table->primary(['propuesta_id','profesor_id']); 
             $table->date('fecha');
             $table->time('hora');
             $table->text('comentario');
-            $table->foreing('propuesta_id')->references('id')->on('propuestas'); //foreing key
-            $table->foreing('profesor_id')->references('id')->on('profesores'); //foreing key
+            $table->foreign('propuesta_id')->references('id')->on('propuestas'); //foreing key
+            $table->foreign('profesor_id')->references('id')->on('profesores'); //foreing key
             //cuando fue creado y modificado por default
             $table->timestamps();
         });
