9e81bf57ef92724d7559bf54e520394cb15bbffb
Bastian <bastian.rojash@usm.cl>
2023-06-04 23:37:43 +0000

    Jun 4, 2023, 7:37 PM

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function iniciar(){
+    public function iniciar(Request $request){ //No es lo mas optimo pero es para que funcione en este caso
         return redirect()->route('admin.index');
     }
     public function index(){
