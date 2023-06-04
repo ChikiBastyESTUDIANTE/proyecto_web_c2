fac333fe2fbdb83f8ee87e40908e006f907c29ce
Bastian <bastian.rojash@usm.cl>
2023-06-04 23:22:30 +0000

    Jun 4, 2023, 7:22 PM

--- database/seeders/EstudianteSeeder.php
+++ database/seeders/EstudianteSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EstudianteSeeder extends Seeder
 {
@@ -12,6 +13,10 @@ class EstudianteSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('estudiantes')->insert([
+            ['rut'=>'21403996-6','nombre'=>'Bastian','apellido'=>'Rojas','email'=>'quierollorar@gmail.com','created_at'=>Carbon::now()],
+            ['rut'=>'21125670-2','nombre'=>'Vania','apellido'=>'Castro','email'=>'quierollorar2@gmail.com','created_at'=>Carbon::now()],
+            ['rut'=>'21471167-2','nombre'=>'Rodrigo','apellido'=>'Plaza','email'=>'quierollorar3@gmail.com','created_at'=>Carbon::now()],
+        ]);
     }
 }
