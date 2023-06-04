8e5348a7d8e540e98db36fd6ff1a83440219c60a
Bastian <bastian.rojash@usm.cl>
2023-06-04 05:10:13 +0000

    Jun 4, 2023, 1:10 AM

--- app/Models/ProfesorPropuesta.php
+++ app/Models/ProfesorPropuesta.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquebt\Relations\HasMany;
 
 class ProfesorPropuesta extends Model
 {
