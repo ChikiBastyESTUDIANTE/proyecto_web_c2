7839d1b572dce4e8f2ec48682acbf7f22ddb61c8
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-06-04 00:42:33 +0000

    Jun 3, 2023, 8:42 PM

--- resources/views/inicio/ingresar.blade.php
+++ resources/views/inicio/ingresar.blade.php
@@ -39,7 +39,7 @@
             <div class="col-5">
                 <div class="bg-primary p-5">
                     <div class="card">
-                        
+                        Lorem Ipsum
                     </div>
                 </div>
             </div>
