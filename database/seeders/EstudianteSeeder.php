5af68e53b37e072bbdef2f63c0d6d8cac8e95c43
Bastian <bastian.rojash@usm.cl>
2023-06-04 23:23:21 +0000

    Jun 4, 2023, 7:23 PM

--- database/seeders/EstudianteSeeder.php
+++ database/seeders/EstudianteSeeder.php
@@ -16,7 +16,7 @@ class EstudianteSeeder extends Seeder
         DB::table('estudiantes')->insert([
             ['rut'=>'21403996-6','nombre'=>'Bastian','apellido'=>'Rojas','email'=>'quierollorar@gmail.com','created_at'=>Carbon::now()],
             ['rut'=>'21125670-2','nombre'=>'Vania','apellido'=>'Castro','email'=>'quierollorar2@gmail.com','created_at'=>Carbon::now()],
-            ['rut'=>'21471167-2','nombre'=>'Rodrigo','apellido'=>'Plaza','email'=>'quierollorar3@gmail.com','created_at'=>Carbon::now()],
+            ['rut'=>'21471167-2','nombre'=>'Rodrigo','apellido'=>'Plaza','email'=>'quierollorar3@gmail.com','created_at'=>Carbon::now()]
         ]);
     }
 }
