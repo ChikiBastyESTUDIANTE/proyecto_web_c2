93e6782132ed3ef5d60e4e83a47093843b852d83
Bastian <bastian.rojash@usm.cl>
2023-06-04 23:36:19 +0000

    Jun 4, 2023, 7:36 PM

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    public function iniciar(){
+        return redirect()->route('admin.index');
+    }
     public function index(){
         return view('administrador.index');
     }
