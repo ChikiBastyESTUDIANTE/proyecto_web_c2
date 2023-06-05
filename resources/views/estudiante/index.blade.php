2a19a6354cffc01f7ca9a713087edf479f34a0ed
Bastian <bastian.rojash@usm.cl>
2023-06-05 07:04:14 +0000

    Jun 5, 2023, 3:04 AM

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -49,9 +49,11 @@
                     </div>
                     <div class="card-body">
                         <form action="" enctype="multipart/form-data">
-                            <label for="archivo"class="form-label"></label>
+                            <label for="archivo" class="form-label"></label>
                             <input type="file" name="archivo" id="archivo" class="form-label">
-                            
+
+                            <label for="enviar" class="form-label"></label>
+                            <button type="submit" class="btn btn-secondary">Subir propuesta</button>
                         </form>
                     </div>
                 </div>
