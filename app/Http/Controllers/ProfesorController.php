ba693d2914493fea98c9447515fb85dd02744e87
Bastian <bastian.rojash@usm.cl>
2023-06-05 02:13:40 +0000

    Jun 4, 2023, 10:13 PM

--- app/Http/Controllers/ProfesorController.php
+++ app/Http/Controllers/ProfesorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Profesor;
+use App\Models\Propuesta;
 
 class ProfesorController extends Controller
 {
@@ -12,6 +13,7 @@ class ProfesorController extends Controller
     }
 
     public function index(Profesor $profesor){
+        $propuestas = Propuesta::orderBy('id')->get();
         return view('profesor.index');
     }
 }
