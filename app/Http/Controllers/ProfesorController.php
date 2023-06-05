fbafa9f2901d289e8c603af3963dce2e30524f1e
Bastian <bastian.rojash@usm.cl>
2023-06-05 03:55:42 +0000

    Jun 4, 2023, 11:55 PM

--- app/Http/Controllers/ProfesorController.php
+++ app/Http/Controllers/ProfesorController.php
@@ -9,9 +9,10 @@ use App\Models\Propuesta;
 class ProfesorController extends Controller
 {
     public function iniciar(Request $request){
+        //return redirect()->route('profesor.menu',$request->id); NO FUNCIONO ESTO TUVE QUE BUSCAR PARA HACER QUE ESTO FUNCIONE
         return redirect()->route('profesor.menu',['id'=>$request->id]);
     }
-
+    //public function index(Profesor $profesor) ASI ESTABA ANTES 
     public function index($id){
         $profesor = Profesor::where('id_rut',$id)->first();
         $propuestas = Propuesta::all();
