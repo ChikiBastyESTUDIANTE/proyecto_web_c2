c653d8aaf82676ba86257edfd37de2591b780d36
Bastian <bastian.rojash@usm.cl>
2023-06-05 04:02:01 +0000

    Jun 5, 2023, 12:02 AM

--- database/seeders/PropuestasSeeder.php
+++ database/seeders/PropuestasSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class PropuestasSeeder extends Seeder
 {
@@ -12,6 +14,8 @@ class PropuestasSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('propuestas')->insert([
+            ['fecha'=>Carbon::now(),'documento'=>'test.txt','estado'=>1,"estudiante_rut"=>'21403996-6']
+        ]);
     }
 }
