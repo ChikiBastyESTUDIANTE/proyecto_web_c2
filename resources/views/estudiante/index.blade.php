a9ca2a983e58de8fa5f54e7637cca8fbc293e96b
Bastian <bastian.rojash@usm.cl>
2023-06-05 04:55:27 +0000

    Jun 5, 2023, 12:55 AM

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -1,1 +1,10 @@
 @extends('templates.templatenav')
+
+@section('codigo-principal')
+    <div class='container min-vh-100'>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col"></div>
+        </div>
+    </div>
+@endsection
