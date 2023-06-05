e866e5589526e41f7225898f9a91cd7809be1c07
Bastian <bastian.rojash@usm.cl>
2023-06-05 03:25:52 +0000

    Jun 4, 2023, 11:25 PM

--- app/Http/Controllers/ProfesorController.php
+++ app/Http/Controllers/ProfesorController.php
@@ -5,16 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Profesor;
 use App\Models\Propuesta;
+use Illuminate\Http\RedirectResponse;
 
 class ProfesorController extends Controller
 {
-    public function iniciar(Request $request){
+    public function iniciar(Request $request): RedirectResponse{
         return redirect()->route('profesor.menu',$request->id);
     }
 
     public function index(Profesor $profesor){
-        return $profesor->id_rut;
-        // $propuestas = Propuesta::all();
-        // return view('profesor.index',compact(['profesor','propuestas']));
+        $propuestas = Propuesta::all();
+        return view('profesor.index',compact(['profesor','propuestas']));
     }
 }
