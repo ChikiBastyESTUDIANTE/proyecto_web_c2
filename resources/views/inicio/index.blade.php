e93a871d49bf5e8bcf6004ed845fa12de65158a6
Bastian <bastian.rojash@usm.cl>
2023-06-05 00:45:11 +0000

    Jun 4, 2023, 8:45 PM

--- resources/views/inicio/index.blade.php
+++ resources/views/inicio/index.blade.php
@@ -34,8 +34,7 @@
                             </div>
                             <div class="text-end p-2">
                                 <a href="{{route('inicio.profesor')}}" class="btn d-flex justify-content-center btn-outline-light">
-                                    <h4>Iniciar como</h4>
-                                    <h4>profesor</h4>
+                                    <h4>Iniciar como aaaprofesor</h4>
                                 </a>
                             </div>
                         </form>
