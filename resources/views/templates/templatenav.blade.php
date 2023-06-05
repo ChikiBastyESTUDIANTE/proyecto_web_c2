de8a0f678a98ad5d9010cacc139263a3b10c30e7
Bastian <bastian.rojash@usm.cl>
2023-06-05 01:28:44 +0000

    Jun 4, 2023, 9:28 PM

--- resources/views/templates/templatenav.blade.php
+++ resources/views/templates/templatenav.blade.php
@@ -8,14 +8,14 @@
     @yield('links')
     <title>Universidad Lorem Ipsum</title>
 </head>
-<body>
+<body class="bg-light">
     {{--  navbar --}}
     <div class="container-fluid bg-primary">
                 <div class="row">
                     <div class="col">
                         <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-dark">
                             <div class="container-fluid">
-                            <h3 class="text-white">Universidad Lorem Ipsum</h3>
+                            <li><h3 class="text-white">Universidad Lorem Ipsum</h3></li>
                             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>
