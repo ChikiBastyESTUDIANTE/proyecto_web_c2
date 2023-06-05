1d2c02567110df6e404c33f6a387e816911afef8
Bastian <bastian.rojash@usm.cl>
2023-06-05 05:13:11 +0000

    Jun 5, 2023, 1:13 AM

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -13,8 +13,9 @@ class EstudianteController extends Controller
     }
 
     public function index($id){
-        $estudiante = Estudiante::where('rut',$id);
-        $propuestas = Propuesta::where('estudiante_rut',$id);
+        $estudiante = Estudiante::where('rut',$id)->first();
+        $propuestas = Propuesta::where('estudiante_rut',$id)->all();
+        dd($propuestas);
         return view('estudiante.index',compact(['estudiante','propuestas']));
     }
 }
