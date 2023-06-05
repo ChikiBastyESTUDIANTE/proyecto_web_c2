809794ca8e43cc3be31dc40a8f036541987e8e53
Bastian <bastian.rojash@usm.cl>
2023-06-05 04:58:28 +0000

    Jun 5, 2023, 12:58 AM

--- resources/views/estudiante/index.blade.php
+++ resources/views/estudiante/index.blade.php
@@ -1,10 +1,27 @@
 @extends('templates.templatenav')
 
 @section('codigo-principal')
-    <div class='container min-vh-100'>
-        <div class="row">
-            <div class="col"></div>
-            <div class="col"></div>
+    <div class="container min-vh-100">
+        <div class="row mt-lg-5">
+            <div class="col-12 col-lg-6">
+                <div class="card">
+                    <div class="card-header bg-secondary">
+                        <h4 class="text-white">Tus propuestas</h4>
+                    </div>
+                    <div class="card-body">
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="card">
+                    <div class="card-header bg-secondary">
+                        <h4 class="text-white">Crear propuesta</h4>
+                    </div>
+                    <div class="card-body">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ut enim blanditiis, alias veritatis, aliquam molestias repellat ab sit maxime facere aperiam expedita non iure aliquid consectetur sed. Quae, aliquam!
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
