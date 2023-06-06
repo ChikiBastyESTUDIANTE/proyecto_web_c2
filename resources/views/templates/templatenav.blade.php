7a32b0f205e5544e73d9c6b1b1ae95e04c37e778
Bastian <bastian.rojash@usm.cl>
2023-06-06 20:35:53 +0000

    Jun 6, 2023, 4:35 PM

--- resources/views/templates/templatenav.blade.php
+++ resources/views/templates/templatenav.blade.php
@@ -40,9 +40,9 @@
                                             @if(Route::current()->getName()=='admin.menu')
                                                 Admin
                                             @elseif(Route::current()->getName()=='estudiante.menu')
-                                                Estudiante {{$estudiante->nombre}} {{$estudiante->apellido}}
+                                                Estudiante
                                             @elseif(Route::current()->getName()=='profesor.menu')
-                                                Profesor {{$profesor->nombre}} {{$profesor->apellido}}
+                                                Profesor
                                             @endif
                                         </span>
                                         <a href="{{route('inicio.index')}}" class="btn btn-secondary d-flex justify-content-end">Cerrar sesion</a>
