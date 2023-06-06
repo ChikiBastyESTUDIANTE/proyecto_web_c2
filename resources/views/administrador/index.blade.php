690ef4f0ebbfab992eda500d52eb74890d8b38eb
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-06-06 21:42:47 +0000

    Jun 6, 2023, 5:42 PM

--- resources/views/administrador/index.blade.php
+++ resources/views/administrador/index.blade.php
@@ -1,1 +1,12 @@
 @extends('templates.templatenav')
+@section('codigo-principal')
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            
+        </div>
+    </div>
+</div>
+
+@endsection
