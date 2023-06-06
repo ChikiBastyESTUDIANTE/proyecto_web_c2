2e1a65046a7887f8bda8e6bd90a57fce8c946e66
Bastian <bastian.rojash@usm.cl>
2023-06-06 20:28:21 +0000

    Jun 6, 2023, 4:28 PM

--- resources/views/profesor/comentarios.blade.php
+++ resources/views/profesor/comentarios.blade.php
@@ -8,23 +8,25 @@
             <div class="col-6 offset-3 d-flex flex-column mt-5">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title text-center">Comentario</h5>
-                            <div class="row">
-                                <div class="col-12">
-                                    Nombre
+                        <form action="">
+                            <h5 class="card-title text-center">Añadir comentario</h5>
+                                <div class="row">
+                                    <div class="col-12">
+                                        Nombre
+                                    </div>
+                                    <div class="col">
+                                        propuesta.pdf
+                                    </div>
                                 </div>
-                                <div class="col">
-                                    propuesta.pdf
-                                </div>
-                            </div>
-                            <form action="{{route('profesor.iniciar')}}" method="POST">
-                            @csrf
-                                <div class="form-group mt-3 mb-3 d-flex">
-                                    <textarea class="form-control" rows="5"></textarea>
-                                </div>
-                                <button type="submit" class="btn btn-primary">Enviar comentario</button>
-                                <button class="btn btn-primary" type="submit">Volver</button>
-                            </form>
+                                <form action="{{route('profesor.iniciar')}}" method="POST">
+                                @csrf
+                                    <div class="form-group mt-3 mb-3 d-flex">
+                                        <textarea class="form-control" rows="5"></textarea>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Enviar comentario</button>
+                                    <button class="btn btn-primary" type="submit">Volver</button>
+                                </form>
+                        </form>
                     </div>
                 </div>
             </div>
