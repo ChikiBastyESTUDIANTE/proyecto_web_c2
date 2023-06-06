013458767d78473899551aaa31eb8019d03daa02
Bastian <bastian.rojash@usm.cl>
2023-06-06 22:20:07 +0000

    Jun 6, 2023, 6:20 PM

--- app/Http/Controllers/ProfesorController.php
+++ app/Http/Controllers/ProfesorController.php
@@ -27,7 +27,8 @@ class ProfesorController extends Controller
         $profesores = Profesor::all();
         return view('profesor.comentarios',compact(['propuesta','profesores']));
     }
-    public function crearComentario(Propuesta $propuesta, Profesor $profesor){
+    public function crearComentario(Propuesta $propuesta, Request $request){
+        dd($request);
         return redirect()->route('profesor.menu');
     }
 }
