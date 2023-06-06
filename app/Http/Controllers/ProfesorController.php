da27cff7d936295bd88f18844221c4c6fce1465d
Bastian <bastian.rojash@usm.cl>
2023-06-06 20:37:54 +0000

    Jun 6, 2023, 4:37 PM

--- app/Http/Controllers/ProfesorController.php
+++ app/Http/Controllers/ProfesorController.php
@@ -8,16 +8,16 @@ use App\Models\Propuesta;
 
 class ProfesorController extends Controller
 {
-    public function iniciar(Request $request){
-        //return redirect()->route('profesor.menu',$request->id); NO FUNCIONO ESTO TUVE QUE BUSCAR PARA HACER QUE ESTO FUNCIONE
-        return redirect()->route('profesor.menu',['id'=>$request->id]);
-    }
-    //public function index(Profesor $profesor) ASI ESTABA ANTES 
-    public function index($id){
-        $profesor = Profesor::where('id_rut',$id)->first();
-        $propuestas = Propuesta::all();
-        return view('profesor.index',compact(['profesor','propuestas']));
-    }
+    // public function iniciar(Request $request){
+    //     //return redirect()->route('profesor.menu',$request->id); NO FUNCIONO ESTO TUVE QUE BUSCAR PARA HACER QUE ESTO FUNCIONE
+    //     return redirect()->route('profesor.menu',['id'=>$request->id]);
+    // }
+    // //public function index(Profesor $profesor) ASI ESTABA ANTES 
+    // public function index($id){
+    //     $profesor = Profesor::where('id_rut',$id)->first();
+    //     $propuestas = Propuesta::all();
+    //     return view('profesor.index',compact(['profesor','propuestas']));
+    // }
 
     public function prueba(){
         return view('profesor.comentarios');
