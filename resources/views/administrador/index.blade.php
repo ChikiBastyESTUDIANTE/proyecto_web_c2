8cc9cfb61223f230cca5f86db30b30e4faaf1c5a
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-06-06 22:18:08 +0000

    Jun 6, 2023, 6:18 PM

--- resources/views/administrador/index.blade.php
+++ resources/views/administrador/index.blade.php
@@ -5,7 +5,37 @@
     <div class="row">
         <div class="col">
             <div class="row mt-lg-5">
-                <div class="col-12">
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-header bg-secondary">
+                            <h4 class="text-white">Profesores</h4>
+                        </div>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Modificar</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Mark</td>
+                                        <td>awdawd@usm.cl</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Jacob</td>
+                                    </tr>
+                                    <tr>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
                     <div class="card">
                         <div class="card-header bg-secondary">
                             <h4 class="text-white">Profesores</h4>
@@ -37,20 +67,8 @@
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col">
-                <div class="col-6">
-                    <div class="card">
-                        <div class="card-header bg-secondary">
-                            <h4 class="text-white">Alumnos</h4>
-                        </div>
-                        <div class="card-body">
+                
 
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
 
