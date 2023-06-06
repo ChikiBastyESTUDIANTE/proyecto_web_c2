4cd534f5a651b272c1a7bfabe844a9a399f07874
Bastian <bastian.rojash@usm.cl>
2023-06-06 20:41:32 +0000

    Jun 6, 2023, 4:41 PM

--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,23 @@ use App\Http\Controllers\EstudianteController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+
 //rutas de inicio de sesion
-
-//PROFE SI VE ESTO, SI ESTO NOS PASA POR NO LEER BIEN EL PDF Y QUERER HACER EL INICIO DE SESION 
-
 Route::get('/',[InicioSesionController::class,'index'])->name('inicio.index');
-// Route::get('/inicio-sesion-admin',[InicioSesionController::class,'ingresarAdmin'])->name('inicio.admin');
-// Route::get('/inicio-sesion-estudiante',[InicioSesionController::class,'ingresarEstudiante'])->name('inicio.estudiante');
-// Route::get('/inicio-sesion-profesor',[InicioSesionController::class,'ingresarProfesor'])->name('inicio.profesor');
 //rutas relacionados a profesor
-//Route::post('/profesor/iniciar',[ProfesorController::class,'iniciar'])->name('profesor.iniciar');
 Route::get('/profesor/menu',[ProfesorController::class,'index'])->name('profesor.menu');
 Route::get('/profesor/comentarios',[ProfesorController::class,'prueba'])->name('profesor.comentarios');
 //rutas relacionados a estudiante
-//Route::post('/estudiante/iniciar',[EstudianteController::class,'iniciar'])->name('estudiante.iniciar');
 Route::get('/estudiante/menu',[EstudianteController::class,'index'])->name('estudiante.menu');
 //rutas relaciondas a administrador
-//Route::post('/admin/iniciar',[AdminController::class,'iniciar'])->name('admin.iniciar'); //No es lo mas optimo pero es para que funcione en este caso
 Route::get('/admin/menu',[AdminController::class,'index'])->name('admin.menu');
+
+//PROFE SI VE ESTO, SI ESTO NOS PASA POR NO LEER BIEN EL PDF Y QUERER HACER EL INICIO DE SESION 
+//RUTAS SIN USAR (TALVEZ SIRVAN EN ALGUN MOMENTO?)
+// Route::get('/inicio-sesion-admin',[InicioSesionController::class,'ingresarAdmin'])->name('inicio.admin');
+// Route::get('/inicio-sesion-estudiante',[InicioSesionController::class,'ingresarEstudiante'])->name('inicio.estudiante');
+// Route::get('/inicio-sesion-profesor',[InicioSesionController::class,'ingresarProfesor'])->name('inicio.profesor');
+//Route::post('/profesor/iniciar',[ProfesorController::class,'iniciar'])->name('profesor.iniciar');
+//Route::post('/estudiante/iniciar',[EstudianteController::class,'iniciar'])->name('estudiante.iniciar');
+//Route::post('/admin/iniciar',[AdminController::class,'iniciar'])->name('admin.iniciar'); //No es lo mas optimo pero es para que funcione en este caso
