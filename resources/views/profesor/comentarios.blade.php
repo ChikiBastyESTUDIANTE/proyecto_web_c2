d043c7d732150bf23b981f33827c4e0e1d4cb1ca
Bastian <bastian.rojash@usm.cl>
2023-06-06 21:45:16 +0000

    Jun 6, 2023, 5:45 PM

--- resources/views/profesor/comentarios.blade.php
+++ resources/views/profesor/comentarios.blade.php
@@ -9,15 +9,24 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title text-center">Añadir comentario</h4>
+                            <form action="" method="POST">
+                            @csrf
                             <div>
                                 <div><h5>Rut del estudiante</h5></div>
                                 <div>{{$propuesta->estudiante_rut}}</div>
                                 <hr>
                                 <div><h5>Documento</h5></div>
                                 <div>{{$propuesta->documento}}</div>
+                                <hr>
+                                {{-- Esto se me hace raro pero asi se hace en este caso? XD --}}
+                                <div><h5>Profesor</h5></div>
+                                <label for=""></label>
+                                <select name="" id="">
+                                    @foreach($profesores as $profesor)
+                                        <option value="{{$profesor->id_rut}}">{{}}</option>
+                                    @endforeach
+                                </select>
                             </div>
-                            <form action="" method="POST">
-                            @csrf
                                 <div class="form-group mt-3 mb-3 d-flex">
                                     <textarea class="form-control" rows="5"></textarea>
                                 </div>
