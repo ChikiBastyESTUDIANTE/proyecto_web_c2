a67d7b24dbb6597470e04132359976eb6934c7c2
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-06-06 21:51:24 +0000

    Jun 6, 2023, 5:51 PM

--- resources/views/administrador/index.blade.php
+++ resources/views/administrador/index.blade.php
@@ -3,33 +3,37 @@
 
 <div class="container">
     <div class="row">
-        <div class="col">
-            <div class="row">
-                <div class="col-6 offset-3 d-flex flex-column mt-5">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">Añadir comentario</h5>
-                                <div class="row">
-                                    <div class="col-12">
-                                        Nombre
-                                    </div>
-                                    <div class="col">
-                                        {{$propuesta->documento}}
-                                    </div>
+            <div class="col-6 offset-3 d-flex flex-column mt-5">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title text-center">Añadir comentario</h4>
+                            <form action="" method="POST">
+                            @csrf
+                            <div>
+                                <div><h5>Rut del estudiante</h5></div>
+                                <div>{{$propuesta->estudiante_rut}}</div>
+                                <hr>
+                                <div><h5>Documento</h5></div>
+                                <div>{{$propuesta->documento}}</div>
+                                <hr>
+                                {{-- Esto se me hace raro pero asi se hace en este caso? XD --}}
+                                <div><h5>Profesor</h5></div>
+                                <label for=""></label>
+                                <select name="" id="">
+                                    @foreach($profesores as $profesor)
+                                        <option value="{{$profesor->id_rut}}">{{}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                                <div class="form-group mt-3 mb-3 d-flex">
+                                    <textarea class="form-control" rows="5"></textarea>
                                 </div>
-                                <form action="" method="POST">
-                                @csrf
-                                    <div class="form-group mt-3 mb-3 d-flex">
-                                        <textarea class="form-control" rows="5"></textarea>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Enviar comentario</button>
-                                    <a href=""  class="btn btn-primary">Volver</a>
-                                </form>
-                        </div>
+                                <button type="submit" class="btn btn-primary">Enviar comentario</button>
+                                <button type="reset" class="btn btn-primary">Volver</button>
+                            </form>
                     </div>
                 </div>
             </div>
-        </div>
         <div class="col">
         
         </div>
