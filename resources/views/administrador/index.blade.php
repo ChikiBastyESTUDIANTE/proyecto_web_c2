9571a59070e7ec86c3ad6c4c0566653f21e1e4ce
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-06-06 22:14:56 +0000

    Jun 6, 2023, 6:14 PM

--- resources/views/administrador/index.blade.php
+++ resources/views/administrador/index.blade.php
@@ -38,14 +38,12 @@
             </div>
         </div>
         <div class="col-6">
-            <div class="col-12 col-lg-6">
-                <div class="card">
-                    <div class="card-header bg-secondary">
-                        <h4 class="text-white">Alumnos</h4>
-                    </div>
-                    <div class="card-body">
+            <div class="card">
+                <div class="card-header bg-secondary">
+                    <h4 class="text-white">Alumnos</h4>
+                </div>
+                <div class="card-body">
 
-                    </div>
                 </div>
             </div>
         </div>
