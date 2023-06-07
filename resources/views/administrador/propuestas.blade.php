20ae75f4a698173cd9c9013dfa641f779c5a0026
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-06-07 00:15:09 +0000

    Jun 6, 2023, 8:15 PM

---
+++ resources/views/administrador/propuestas.blade.php
@@ -0,0 +1,84 @@
+@extends('templates.templatenav')
+@section('links')
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+@endsection
+@section('codigo-principal')
+
+<div class="container min-vh-100">
+    <div class="row">
+        <div class="col">
+            <div class="row mt-lg-5">
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-header bg-secondary">
+                            <h4 class="text-white">Profesores</h4>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Modificar</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Mark</td>
+                                        <td>awdawd@usm.cl</td>
+                                        <td>
+                                            <a class="btn btn-sm btn-warning pt-2 text-white" href="">
+                                            <span class="material-symbols-outlined">edit</span>
+                                            </a>
+                                            <a class="btn btn-sm btn-danger pt-2 text-white" href="">
+                                            <span class="material-symbols-outlined">delete</span>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-header bg-secondary">
+                            <h4 class="text-white">Alumnos</h4>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">Rut</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Modificar</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Mark</td>
+                                        <td>22.222.222-2</td>
+                                        <td>awdawd@usm.cl</td>
+                                        <td>
+                                            <a class="btn btn-sm btn-warning pt-2 text-white" href="">
+                                            <span class="material-symbols-outlined">edit</span>
+                                            </a>
+                                            <a class="btn btn-sm btn-danger pt-2 text-white" href="">
+                                            <span class="material-symbols-outlined">delete</span>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+                
+
+    </div>
+</div>
+
+@endsection
