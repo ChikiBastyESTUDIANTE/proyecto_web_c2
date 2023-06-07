0b43a674c8e3f865b5a6162309db10e7874131c9
Bastian <bastian.rojash@usm.cl>
2023-06-07 21:55:55 +0000

    Jun 7, 2023, 5:55 PM

--- resources/views/profesor/confirmar_delete.blade.php
+++ resources/views/profesor/confirmar_delete.blade.php
@@ -8,21 +8,23 @@
             <div class="col-6 offset-3 d-flex flex-column mt-5">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title text-center">Elija su perfil de profesor</h4>
+                        <h4 class="card-title text-center">Eliminar comentario</h4>
                             <form action="{{route('profesor.crear.comentario',$propuesta->id)}}" method="POST">
                             @csrf
                             <div>
-                                <div><h5>Rut del estudiante</h5></div>
-                                <div>{{$propuesta->estudiante_rut}}</div>
-                                <hr>
-                                <div><h5>Documento</h5></div>
-                                <div>{{$propuesta->documento}}</div>
-                                <hr>
-                                <div><h5>Profesor</h5></div>
-                                <div>{{$profesor->nombre}} {{$profesor->apellido}} (id: {{$profesor->id}})</div>
-                                <hr>
+                                <div class="row">
+                                    <div><h5>Rut del estudiante</h5></div>
+                                    <div>{{$propuesta->estudiante_rut}}</div>
+                                    <hr>
+                                    <div><h5>Documento</h5></div>
+                                    <div>{{$propuesta->documento}}</div>
+                                    <hr>
+                                    <div><h5>Profesor</h5></div>
+                                    <div>{{$profesor->nombre}} {{$profesor->apellido}} (id: {{$profesor->id}})</div>
+                                    <hr>
+                                </div>
                             </div>
-                                <button type="submit" class="btn btn-primary">Aceptar</button>
+                                <button type="submit" class="btn btn-primary">Eliminar</button>
                                 <a href='{{route('profesor.menu')}}' class="btn btn-primary">Volver</a>
                             </form>
                     </div>
