7c88aa966865359169ea65c9524f29fa01f02349
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-06-07 01:25:47 +0000

    Jun 6, 2023, 9:25 PM

--- resources/views/administrador/propuestas.blade.php
+++ resources/views/administrador/propuestas.blade.php
@@ -3,16 +3,42 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 @endsection
 @section('codigo-principal')
-    <div class="container min-vh-100">
-        <div class="row">
-            <div class="col-6 offset-3 d-flex flex-column mt-5">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title text-center">Añadir comentario</h4>
-                            
+<div class="container min-vh-100">
+    <div class="row">
+        <div class="col">
+            <div class="row mt-lg-5">
+                <div class="col-6">
+                    <div class="card">
+                        <div class="card-header bg-secondary">
+                            <h4 class="text-white">Profesores</h4>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Nombre</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Modificar</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Mark</td>
+                                        <td>awdawd@usm.cl</td>
+                                        <td>
+                                            <a class="btn btn-sm btn-warning pt-2 text-white" href="">
+                                            <span class="material-symbols-outlined">edit</span>
+                                            </a>
+                                            <a class="btn btn-sm btn-danger pt-2 text-white" href="">
+                                            <span class="material-symbols-outlined">delete</span>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </div>    
 @endsection
