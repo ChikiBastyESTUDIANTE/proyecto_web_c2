8a6ba9cd6cb3e3f348de0b9084dd544678b3e110
Bastian <bastian.rojash@usm.cl>
2023-06-07 01:17:04 +0000

    Jun 6, 2023, 9:17 PM

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('/profesor/comentarios/{propuesta}',[ProfesorController::class,'comen
 Route::post('/profesor/comentarios/generar/{propuesta}',[ProfesorController::class,'crearComentario'])->name('profesor.crear.comentario');
 //rutas relacionados a estudiante
 Route::get('/estudiante/menu',[EstudianteController::class,'menu'])->name('estudiante.menu');
+Route::get('/estudiante/login',[EstudianteController::class,'login'])->name('estudiante.login');
 //rutas relaciondas a administrador
 Route::get('/admin/administrar',[AdminController::class,'menu'])->name('admin.menu');
 Route::get('/admin/propuestas',[AdminController::class,'propuestas'])->name('admin.propuestas');
