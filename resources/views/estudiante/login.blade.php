8e327d1e42fdb69e5c0ad7fff3f670d16715859b
Bastian <bastian.rojash@usm.cl>
2023-06-07 01:23:10 +0000

    Jun 6, 2023, 9:23 PM

--- resources/views/estudiante/login.blade.php
+++ resources/views/estudiante/login.blade.php
@@ -1,20 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('paleta-colores/paleta.css')}}">
-    @yield('links')
-    <title>Universidad Lorem Ipsum</title>
-</head>
-<body class="bg-light">
+@extends('templates.templatenav')
+@section('codigo-principal')
     <div class="container">
-        <div class="row">
+        <div class="row mt-lg-5">
             <div class="col-6 offset-3"> 
                 <div class="card">
                     <div class="card-body">
-                        <form action="" class="form-controll">
+                        <form action="" class="for">
                             @foreach($estudiantes as $estudiante)
                                 <option value="{{$estudiante->rut}}">{{$estudiante->nombre}}{{$estudiante->apellido}}{{$estudiante->rut}}</option>
                             @endforeach
@@ -24,4 +15,4 @@
             </div>
         </div>
     </div>
-</body>
+@endsection
