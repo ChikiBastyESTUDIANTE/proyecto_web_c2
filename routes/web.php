7127d19d7b27c6635f1246e4ee860a1705c8350c
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-06-08 00:22:54 +0000

    Jun 7, 2023, 8:22 PM

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::post('/estudiante/login',[EstudianteController::class,'conseguirDatos'])-
 //rutas relaciondas a administrador
 Route::get('/admin/administrar',[AdminController::class,'menu'])->name('admin.menu');
 Route::get('/admin/propuestas',[AdminController::class,'propuestas'])->name('admin.propuestas');
+Route::get('/admin/estado',[AdminController::class,'estado'])->name('admin.estado');
 //PROFE SI VE ESTO, SI ESTO NOS PASA POR NO LEER BIEN EL PDF Y QUERER HACER EL INICIO DE SESION 
 //RUTAS SIN USAR (TALVEZ SIRVAN EN ALGUN MOMENTO?)
 // Route::get('/inicio-sesion-admin',[InicioSesionController::class,'ingresarAdmin'])->name('inicio.admin');
