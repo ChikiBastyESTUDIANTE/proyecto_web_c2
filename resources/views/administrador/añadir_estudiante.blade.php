9fe9d9a42588d89b416d73221043dc0b5c99c402
Bastian <bastian.rojash@usm.cl>
2023-06-09 00:36:00 +0000

    Jun 8, 2023, 8:36 PM

--- resources/views/administrador/añadir_estudiante.blade.php
+++ resources/views/administrador/añadir_estudiante.blade.php
@@ -8,7 +8,7 @@
             <div class="col-6 offset-3 d-flex flex-column mt-5">
                 <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title text-center">Añadir estudiante</h4>
+                        <h3 class="card-title text-center">Añadir estudiante</h3>
                             <form action="{{route('admin.menu')}}" method="POST">
                             @csrf
                             <div class="mb-3">
