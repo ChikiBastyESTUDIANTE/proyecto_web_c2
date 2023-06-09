eb443b046900aafc7450fb1473fd796cecdcab56
Bastian <bastian.rojash@usm.cl>
2023-06-09 01:06:05 +0000

    Jun 8, 2023, 9:06 PM

--- routes/web.php
+++ routes/web.php
@@ -37,8 +37,6 @@ Route::get('/admin/estado/{propuesta}',[AdminController::class,'estado'])->name(
 Route::post('/admin/estado/{propuesta}',[AdminController::class,'cambiarEstado'])->name('admin.cambiarEstado');
 Route::get('/admin/añadir/estudiante',[AdminController::class,'añadirEstudiante'])->name('admin.añadirEstudiante');
 Route::get('/admin/añadir/profesor',[AdminController::class,'añadirProfesor'])->name('admin.añadirProfesor');
-Route::get('/admin/modificar/estudiante/{estudiante}',[AdminController::class,'modificarEstudiante'])->name('admin.modificarEstudiante');
-Route::get('/admin/modificar/profesor/{profesor}',[AdminController::class,'modificarProfesor'])->name('admin.modificarProfesor');
 //no segui las convenciones de los nombres ups no me rete profe :(
 //PROFE SI VE ESTO, SI ESTO NOS PASA POR NO LEER BIEN EL PDF Y QUERER HACER EL INICIO DE SESION 
 //RUTAS SIN USAR (TALVEZ SIRVAN EN ALGUN MOMENTO?)
