ad343686336c9e159432d714374905a5d01992e6
Bastian <bastian.rojash@usm.cl>
2023-06-09 07:26:19 +0000

    Jun 9, 2023, 3:26 AM

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -37,7 +37,7 @@ class EstudianteController extends Controller
 
 
     public function subirArchivo($estudiante,Request $request){
-        $request->file('archivo')->storeAs('archivos_propuestas',$request->file('archivo')->getClientOriginalName()); //PROFE NO SE LEER, NO ENCONTRABA LO DE getClientOriginalName() EN LA DOCUMENTACION DE LARAVEL CASI ME VUELVO LOCO BUSCANDO ESTO
+        $request->file('archivo')->storeAs('',$request->file('archivo')->getClientOriginalName()); //PROFE NO SE LEER, NO ENCONTRABA LO DE getClientOriginalName() EN LA DOCUMENTACION DE LARAVEL CASI ME VUELVO LOCO BUSCANDO ESTO
         $propuesta = new Propuesta();
         $propuesta->documento = $request->file('archivo')->getClientOriginalName();
         $propuesta->fecha = Carbon::now();
