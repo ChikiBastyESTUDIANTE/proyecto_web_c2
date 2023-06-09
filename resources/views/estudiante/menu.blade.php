c3c426a67dbdfa2752d4c28b4718843af95f65f6
Bastian <bastian.rojash@usm.cl>
2023-06-09 03:57:59 +0000

    Jun 8, 2023, 11:57 PM

--- resources/views/estudiante/menu.blade.php
+++ resources/views/estudiante/menu.blade.php
@@ -65,7 +65,8 @@
                         <h3 class="text-white pt-2">Crear propuesta</h3>
                     </div>
                     <div class="card-body">
-                        <form action="" enctype="multipart/form-data">
+                        <form action="" enctype="multipart/form-data" method="POST">
+                            @csrf
                             <div><input type="file" name="archivo" id="archivo" class="form-control"></div>
                             <div class="mt-3"><button type="submit" class="btn btn-primary-rojo">Subir propuesta</button></div>
                         </form>
