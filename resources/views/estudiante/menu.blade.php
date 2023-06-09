32f75b4a20928eefa5db760a49d00b68f05b98b0
Bastian <bastian.rojash@usm.cl>
2023-06-09 02:42:01 +0000

    Jun 8, 2023, 10:42 PM

--- resources/views/estudiante/menu.blade.php
+++ resources/views/estudiante/menu.blade.php
@@ -43,7 +43,7 @@
                                                 @endforeach --}}
                                                 <a href="" class="btn btn-outline-secondary d-flex flex-column align-items-center justify-content-center mx-2 ms-lg-3" style="width:10rem; height:3rem">Ver comentarios</a>
                                             @else
-                                                <a href="" class="btn btn-outline-primary disabled d-flex align-items-center mx-2 ms-lg-3" style="width:10rem; height:3rem">No hay comentarios</a>
+                                                <a href="" class="btn btn-outline-primary disabled d-flex flex-column align-items-center justify-content-center mx-2 ms-lg-3" style="width:10rem; height:3rem">No hay comentarios</a>
                                             @endif
                                         </td>
                                             {{-- @if(count($propuesta->comentarioProfesorConPivot)>0)
