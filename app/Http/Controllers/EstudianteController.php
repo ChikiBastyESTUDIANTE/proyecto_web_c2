d2598b022e009fae2c08a40ff1767ad80ec0b5c1
Bastian <bastian.rojash@usm.cl>
2023-06-09 06:19:27 +0000

    Jun 9, 2023, 2:19 AM

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -37,7 +37,7 @@ class EstudianteController extends Controller
 
 
     public function subirArchivo($estudiante,Request $request){
-        $request->file('archivo')->storeAs('','prueba.'.$request->extension());
+        $request->file('archivo')->store('archivos_propuestas');
         $propuesta = new Propuesta();
         $propuesta->documento = $request->file('archivo');
         $propuesta->fecha = Carbon::now();
