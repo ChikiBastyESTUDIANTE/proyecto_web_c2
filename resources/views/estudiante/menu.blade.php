b0c7467100058c86f1614d8f7a5612fe5cafa21f
Bastian <bastian.rojash@usm.cl>
2023-06-10 01:42:57 +0000

    Jun 9, 2023, 9:42 PM

--- resources/views/estudiante/menu.blade.php
+++ resources/views/estudiante/menu.blade.php
@@ -4,7 +4,7 @@
     <div class="container min-vh-100">
         <div class="row mt-lg-5">
             {{-- VER TUS PROPUESTAS --}}
-            <div class="col-12 col-lg-6">
+            <div class="col-12 col-lg-8">
                 <div class="card">
                     <div class="card-header bg-secondary">
                         <h3 class="text-white pt-2">Tus propuestas</h3>
@@ -40,18 +40,12 @@
                                                 @foreach($propuesta->comentarioProfesorConPivot->where('pivot.propuesta_id',$propuesta->id) as $comentario)
                                                 {{$comentario->pivot->comentario}}
                                                 <br>
+                                                <br>
                                                 @endforeach
-                                                {{-- <a href="" class="btn btn-outline-secondary">Ver comentarios</a> --}}
                                             @else
-                                                {{-- <a href="" class="btn btn-outline-primary disabled">No hay comentarios</a> --}}
                                                 No hay comentarios
                                             @endif
                                         </td>
-                                            {{-- @if(count($propuesta->comentarioProfesorConPivot)>0)
-                                                <td>{{$propuesta->comentarioProfesorConPivot->where('pivot.propuesta_id',$propuesta->id)->first()->pivot->comentario}}</td>
-                                            @else
-                                                <td>No hay comentarios</td>
-                                            @endif --}}
                                     </tr>
                                 @endforeach
                             </tbody>
@@ -60,7 +54,7 @@
                 </div>
             </div>
             {{-- CREAR PROPUESTA --}}
-            <div class="col-12 col-lg-6">
+            <div class="col-12 col-lg-4">
                 <div class="card">
                     <div class="card-header bg-secondary-rojo">
                         <h3 class="text-white pt-2">Crear propuesta</h3>
@@ -69,7 +63,7 @@
                         <form action="{{route('estudiante.subirArchivo',$estudiante->rut)}}" enctype="multipart/form-data" method="POST">
                             @csrf
                             <div><input type="file" name="archivo" id="archivo" class="form-control"></div>
-                            <div class="mt-3"><button type="submit" class="btn btn-primary-rojo">Subir propuesta</button></div>
+                            <div class="mt-3"><button type="submit" class="btn btn-secondary-rojo">Subir propuesta</button></div>
                         </form>
                     </div>
                 </div>
