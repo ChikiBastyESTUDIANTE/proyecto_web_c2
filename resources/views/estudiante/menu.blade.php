a6b0f07b13bc7cdfc189673fde074b25b70bb478
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-07-01 07:43:57 +0000

    Jul 1, 2023, 3:43 AM

--- resources/views/estudiante/menu.blade.php
+++ resources/views/estudiante/menu.blade.php
@@ -61,8 +61,7 @@
                         <h3 class="text-white pt-2">Crear propuesta</h3>
                     </div>
                     <div class="card-body">
-                        <form action="{{route('estudiante.subirArchivo',$estudiante->rut)}}" enctype="multipart/form-data" method="POST">
-                            @csrf
+                        <form action="" method="POST">
                             <div><input type="file" name="archivo" id="archivo" class="form-control"></div>
                             <div class="mt-3"><button type="submit" class="btn btn-secondary-rojo">Subir propuesta</button></div>
                         </form>
